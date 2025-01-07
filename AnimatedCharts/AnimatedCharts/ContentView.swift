dec74b71bf44f80ca738428a3acc639f7450cd7e
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-01-07 19:37:33 +0700

    add picker
    add picker and auto trigger animation when changing chart style

--- AnimatedCharts/AnimatedCharts/ContentView.swift
+++ AnimatedCharts/AnimatedCharts/ContentView.swift
@@ -18,39 +18,66 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             VStack {
-                
-                Chart {
-                    ForEach(appDownloads) { download in
-                        BarMark(
-                            x: .value("Month",download.month),
-                            y: .value("Downloads",download.isAnimated ?  download.value : 0)
-                        )
-                        .foregroundStyle(.green.gradient)
-                        .interpolationMethod(.catmullRom)
-                        .lineStyle(.init(lineWidth: 2))
-                        .opacity(download.isAnimated ? 1 : 0)
-                        .symbol {
-                            Circle()
-                                .fill(.green)
-                                .frame(width: 12, height: 12)
-                        }
+                Picker(selection: $selected, label: Text(""), content: {
+                               Text("Bar").tag(0)
+                               Text("Line").tag(1)
+                               Text("Pie").tag(2)
+                           }).pickerStyle(SegmentedPickerStyle())
+                    .onChange(of: selected, initial: false) { oldValue, newValue in
                         
-                    
+                        switch newValue {
+                        case 0:
+                            print("Bar")
+                        case 1:
+                            print("Line")
+                        case 2:
+                            print("Pie")
+                        default:
+                            print("Empty")
+                        }
                     }
-                    
-                }
-                .chartYScale(domain: 0...12000)
-                .frame(height: 250)
-                .padding()
-                .background(.background, in: .rect(cornerRadius: 10))
+    
+                switch selected {
+                                case 0:
+                                    BarView(appDownloads: $appDownloads, isAnimated: $isAnimated, trigger: $trigger)
+                        .chartYScale(domain: 0...12000)
+                        .frame(height: 250)
+                        .padding()
+                        .background(.background, in: .rect(cornerRadius: 10))
+                        
+                        Spacer(minLength: 0)
+                                case 1:
+                    LineView(appDownloads: $appDownloads, isAnimated: $isAnimated, trigger: $trigger)
+                        .chartYScale(domain: 0...12000)
+                        .frame(height: 250)
+                        .padding()
+                        .background(.background, in: .rect(cornerRadius: 10))
+                        
+                        Spacer(minLength: 0)
+                                case 2:
+                    PieView(appDownloads: $appDownloads, isAnimated: $isAnimated, trigger: $trigger)
+                        .chartYScale(domain: 0...12000)
+                        .frame(height: 250)
+                        .padding()
+                        .background(.background, in: .rect(cornerRadius: 10))
+                        
+                        Spacer(minLength: 0)
+                                default:
+                                    EmptyView()
+                                }
+                            
                 
-                Spacer(minLength: 0)
+                
             }
             .padding()
             .background(.gray.opacity(0.12))
             .navigationTitle("Animated Chart's")
             .onAppear(perform: animateChart)
             .onChange(of: trigger, initial: false) { oldValue, newValue in
+                resetChartAnimation()
+                animateChart()
+            }
+            .onChange(of: selected, initial: false) { oldValue, newValue in
                 resetChartAnimation()
                 animateChart()
             }
@@ -64,6 +91,86 @@ struct ContentView: View {
         }
     }
     
+    struct BarView: View {
+        @Binding var appDownloads: [Download]
+        @Binding var isAnimated: Bool
+        @Binding var trigger: Bool
+
+        var body: some View {
+            Chart {
+                ForEach(appDownloads) { download in
+                    BarMark(
+                        x: .value("Month", download.month),
+                        y: .value("Downloads", download.isAnimated ? download.value : 0)
+                    )
+                    .foregroundStyle(.green.gradient)
+                    .interpolationMethod(.catmullRom)
+                    .lineStyle(.init(lineWidth: 2))
+                    .opacity(download.isAnimated ? 1 : 0)
+                    .symbol {
+                        Circle()
+                            .fill(.green)
+                            .frame(width: 12, height: 12)
+                    }
+                }
+            }
+            .chartYScale(domain: 0...12000)
+            .frame(height: 250)
+            .padding()
+            .background(.background, in: .rect(cornerRadius: 10))
+        }
+    }
+    
+    struct LineView: View {
+        @Binding var appDownloads: [Download]
+        @Binding var isAnimated: Bool
+        @Binding var trigger: Bool
+
+        var body: some View {
+            Chart {
+                ForEach(appDownloads) { download in
+                    LineMark(
+                        x: .value("Month", download.month),
+                        y: .value("Downloads", download.isAnimated ? download.value : 0)
+                    )
+                    .foregroundStyle(.blue)
+                    .interpolationMethod(.catmullRom)
+                    .lineStyle(.init(lineWidth: 2))
+                    .opacity(download.isAnimated ? 1 : 0)
+                    .symbol {
+                        Circle()
+                            .fill(.blue)
+                            .frame(width: 8, height: 8)
+                    }
+                }
+            }
+            .chartYScale(domain: 0...12000)
+            .frame(height: 250)
+            .padding()
+            .background(.background, in: .rect(cornerRadius: 10))
+        }
+    }
+
+    struct PieView: View {
+        @Binding var appDownloads: [Download]
+        @Binding var isAnimated: Bool
+        @Binding var trigger: Bool
+        
+        var body: some View {
+            Chart {
+                ForEach(appDownloads) { download in
+                    SectorMark(
+                        angle: .value("Downloads", download.isAnimated ? download.value : 0)
+                    )
+                    .foregroundStyle(by: .value("Month", download.month))
+                }
+            }
+            .chartLegend(.visible)
+            .frame(height: 250)
+            .padding()
+            .background(.background, in: .rect(cornerRadius: 10))
+        }
+    }
     private func animateChart() {
         guard !isAnimated else { return }
         isAnimated = true
